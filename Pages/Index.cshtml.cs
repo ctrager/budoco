1020e8c0cdb9e4e3b1c45d3db3b1f97f5407681c
corey <ctrager@yahoo.com>
2020-10-27 21:26:16 -0500

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+
+namespace net_razor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string foo;
+        
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Console.WriteLine("corey was here");
+            foo = "bar nice 23 xxx 4";
+        }
+        public string GetSomeHtml()
+        {
+            Console.WriteLine("this is GetSomeHtml");
+            return "<h1>corey was here</h1>";    
+        }
+    }
+}
