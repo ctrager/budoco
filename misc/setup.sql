a1992fbf963c8c1d898cbf8843484fe6b8ccb04b
corey <ctrager@yahoo.com>
2020-11-19 19:45:48 -0600

    lots of create tables

--- misc/setup.sql
+++ misc/setup.sql
@@ -1,13 +1,21 @@
 
 drop table if exists users;
+drop table if exists sessions;
+drop table if exists issues;
+drop table if exists projects;
+drop table if exists categories;
+drop table if exists priorities;
+drop table if exists statuses;
+drop table if exists queries;
+drop table if exists posts;
 
 create table users
 (
 us_id serial,
 us_username varchar(20) not null,
 us_email varchar(40) not null,
-us_is_admin boolean,
-us_password varchar(32),
+us_is_admin boolean default false,
+us_password varchar(32) not null default '',
 us_create_timestamp timestamptz default CURRENT_TIMESTAMP not null
 );
 
@@ -23,37 +31,118 @@ create table sessions
 
 CREATE UNIQUE INDEX se_id_index ON sessions (se_id);
 
+create table projects
+(
+pj_id serial,
+pj_name varchar(30) not null,
+pj_default int not null default 0,
+pj_sort_seq int not null default 0
+);
 
-/*
+CREATE UNIQUE INDEX pj_name_index ON projects (pj_name);
 
-us_salt int null,
-us_password varchar(64) not null,
-us_firstname nvarchar(60) null,
-us_lastname nvarchar(60) null,
-us_email nvarchar(120) null,
-us_admin int not null default(0),
-us_default_query int not null default(0),
-us_enable_notifications int not null default(1),
-us_auto_subscribe int not null default(0),
-us_auto_subscribe_own_bugs int null default(0),
-us_auto_subscribe_reported_bugs int null default(0),
-us_send_notifications_to_self int null default(0),
-us_active int not null default(1),
-us_bugs_per_page int null,
-us_forced_project int null,
-us_reported_notifications int not null default(4),
-us_assigned_notifications int not null default(4),
-us_subscribed_notifications int not null default(4),
-us_signature nvarchar(1000) null,
-us_use_fckeditor int not null default(0),
-us_created_user int not null default(1),
-us_org int not null default(1),
-us_most_recent_login_datetime datetime null
-*/
+create table categories
+(
+ca_id serial,
+ca_name varchar(30) not null,
+ca_default int not null default 0,
+ca_sort_seq int not null default 0
+);
+
+CREATE UNIQUE INDEX ca_name_index ON categories (ca_name);
+
+create table statuses
+(
+st_id serial,
+st_name varchar(30) not null,
+st_default int not null default 0,
+st_sort_seq int not null default 0
+);
+
+CREATE UNIQUE INDEX st_name_index ON statuses (st_name);
+
+create table priorities
+(
+pr_id serial,
+pr_name varchar(30) not null,
+pr_default int not null default 0,
+pr_sort_seq int not null default 0
+);
+
+CREATE UNIQUE INDEX pr_name_index ON priorities (pr_name);
+
+create table queries
+(
+qu_id serial,
+qu_desc varchar(60) not null,
+qu_sql text not null,
+qu_default int not null default 0,
+qu_sort_seq int not null default 0
+);
+
+create unique index qu_desc_index on queries (qu_desc);
 
 insert into users (us_username, us_email, us_is_admin, us_password) values('admin', '', true, '');
 insert into users (us_username, us_email, us_is_admin, us_password) values('corey', 'ctrager@gmail.com', true, '');
 insert into users (us_username, us_email, us_is_admin, us_password) values('misayo', 'm@example.com', false, 'x');
 insert into users (us_username, us_email, us_is_admin, us_password) values('abi', 'a@example.com', false, 'x');
 insert into users (us_username, us_email, us_is_admin, us_password) values('isaac', 'i@example.com', false, 'x');
+
+insert into projects (pj_name) values ('proj 1');
+insert into projects (pj_name) values ('proj 2');
+insert into projects (pj_name) values ('proj 3');
+
+insert into categories (ca_name) values ('bug');
+insert into categories (ca_name) values ('task');
+insert into categories (ca_name) values ('question');
+
+insert into priorities (pr_name) values ('1-high');
+insert into priorities (pr_name) values ('2-medium');
+insert into priorities (pr_name) values ('3-low');
+
+insert into statuses (st_name, st_default) values ('new', 1);
+insert into statuses (st_name) values ('in progress');
+insert into statuses (st_name) values ('done');
+
+create table issues 
+(
+is_id serial,
+is_desc varchar(200) not null,
+is_created_by_user int not null,
+is_created_date timestamptz default CURRENT_TIMESTAMP,
+is_status int not null,
+is_priority int not null,
+is_category int not null,
+is_project int not null,
+is_assigned_to_user int null,
+is_last_updated_user int null,
+is_last_updated_date timestamptz null
+);
+
+
+
+create table posts 
+(
+	po_id serial,
+	po_issue int,
+	po_desc text,
+	po_created_by_user int not null,
+	po_created_date timestamptz default CURRENT_TIMESTAMP
+);
+
+create index pst_bug_index on posts (po_issue);
+
+insert into queries (qu_desc, qu_sql, qu_default) values (
+'all issues',
+'select is_id, is_desc, is_project, is_category, is_status, is_priority'
+|| chr(10) || ' from issues '
+|| chr(10) || ' order by is_id desc',
+1);
+
+insert into queries (qu_desc, qu_sql) values (
+'just with category',
+'select is_id, is_desc, is_created_date '
+|| chr(10) || ' from issues '
+|| chr(10) || ' where is_category != 0 '
+|| chr(10) || ' order by is_id desc');
 
