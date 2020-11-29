0ecdff24d73f6e010d3ae618bb13d28c76a0130f
corey <ctrager@yahoo.com>
2020-11-29 10:08:11 -0600

    testing deploy

--- misc/deploy_to_production.sh
+++ misc/deploy_to_production.sh
@@ -4,5 +4,7 @@ dotnet publish -o ../publish_next
 # timestamp it
 cd ..
 touch publish_next/$( date '+%Y-%m-%d_%H-%M-%S' )
+rm -rf publish_prev
 mv publish publish_prev
 mv publish_next publish
+systemctl restart budoco
